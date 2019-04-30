a40f48f6a61ccf029e791c009510518901ce8bb0
!Q0p1q0p <vishnu.n@qentelli.com>
2019-04-30 05:30:01 +0000

    updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,219 +1,218 @@
 "use strict";
 
-var rfc = require("node-rfc");
-var fs = require("fs");
-
 module.exports = function(grunt) {
-
-    // Project specific variables
-    var abapDevelopmentUser = process.env.ABAP_DEVELOPMENT_USER;
-    var abapDevelopmentPassword = process.env.ABAP_DEVELOPMENT_PASSWORD;
-    var abapDevelopmentServer = process.env.ABAP_DEVELOPMENT_SERVER;
-    var abapDevelopmentInstance = process.env.ABAP_DEVELOPMENT_INSTANCE;
-    var abapDevelopmentClient = process.env.ABAP_DEVELOPMENT_CLIENT;
-    var abapApplicationName = process.env.ABAP_APPLICATION_NAME;
-    var abapApplicationDesc = process.env.ABAP_APPLICATION_DESC;
-    var abapPackage = process.env.ABAP_PACKAGE;
-    var jobURL = process.env.JOB_URL;
+    // Variables from environment
+    var nexusUser = process.env.NEXUS_DEPLOY_USER;
+    var nexusPassword = process.env.NEXUS_DEPLOY_PASSWORD;
     var nexusSnapshotRepoURL = process.env.NEXUS_SNAPSHOT_REPO;
-    var gitCommit = process.env.GIT_COMMIT;
 
-    // Global Variables
+    // Project properties
+    var webAppDir = "webapp";
     var targetDir = "target";
+    var tmpDir = targetDir + "/tmp";
+    var tmpDirDbg = targetDir + "/tmp-dbg";
     var zipFileSuffix = "-opt-static-abap.zip";
-    var ctsDataFile = targetDir + "/CTS_Data.txt";
+    var preloadPrefix = "nw/epm/refapps/ext/shop";
     var nexusGroupId = "com.yourcompany";
 
     // Project configuration.
-    var abapConn = {
-        user: abapDevelopmentUser,
-        passwd: abapDevelopmentPassword,
-        ashost: abapDevelopmentServer,
-        sysnr: abapDevelopmentInstance,
-        client: abapDevelopmentClient
-    };
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
-        createTransportRequest: {
+        clean: {
+            build: [targetDir]
+        },
+        encoding: {
+             options: {
+                 encoding: "UTF8"
+             },
+             files: {
+                 src: [webAppDir + "/**/*.js", webAppDir + "/**/*.css",
+                      webAppDir + "/**/*.xml", webAppDir + "/**/*.json",
+                       webAppDir + "/**/*.html", webAppDir + "/**/*.properties"]
+             }
+        },
+        eslint: {
             options: {
-                conn: abapConn,
-                author: abapDevelopmentUser,
-                description: "Commit: " + gitCommit
+                configFile: ".eslintrc.json"
+            },
+            target: [webAppDir + "/**/*.js"]
+        },
+        copy: {
+            copyToDbg: {
+                files: [
+                    {
+                        expand: true,
+                        src: "**/*.js",
+                        dest: tmpDirDbg,
+                        cwd: webAppDir,
+                        filter: function(filepath) {
+                            // prevent js from localService to be copied
+                            return !filepath.match(new RegExp(webAppDir + "(\\/|\\\\)localService", "gi"));
+                        }
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.css",
+                        dest: tmpDirDbg,
+                        cwd: webAppDir
+                    }]
+            },
+            copyToTmp: {
+                files: [
+                    {
+                        expand: true,
+                        src: "**/*.js",
+                        dest: tmpDir,
+                        cwd: webAppDir,
+                        filter: function(filepath) {
+                            // prevent js from localService to be copied
+                            return !filepath.match(new RegExp(webAppDir + "(\\/|\\\\)localService", "gi"));
+                        }
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.css",
+                        dest: tmpDir,
+                        cwd: webAppDir
+                    },
+                    {
+                        expand: true,
+                        src: "localService/metadata.xml",
+                        dest: tmpDir,
+                        cwd: webAppDir
+                    },
+                    {
+                        expand: true,
+                        src: "**/*",
+                        dest: tmpDir,
+                        cwd: webAppDir,
+                        filter: function(filepath) {
+                            // prevent js and css files and contents of webapp/test from being copied
+                            return !filepath.match(new RegExp("(" + webAppDir + "(\\/|\\\\)test|${webAppDir}(\\/|\\\\)localService|\\.js$|\\.css$|\\test.html$)", "gi"));
+                        }
+                    }]
+            },
+            copyDbgToTmp: {
+                files: [
+                    {
+                        expand: true,
+                        src: "**/*.js",
+                        dest: tmpDir,
+                        cwd: tmpDirDbg,
+                        rename: function(dest, src) {
+                            return dest + "/" + src.replace(/((\.view|\.fragment|\.controller)?\.js)/, "-dbg$1");
+                        }
+                    },
+                    {
+                        expand: true,
+                        src: "**/*.css",
+                        dest: tmpDir,
+                        cwd: tmpDirDbg,
+                        rename: function(dest, src) {
+                            return dest + "/" + src.replace(".css", "-dbg.css");
+                        }
+                    }]
             }
         },
-        uploadToABAP: {
-            options: {
-                conn: abapConn,
-                zipFile: targetDir + "/<%= pkg.name %>" + zipFileSuffix,
-                zipFileURL: nexusSnapshotRepoURL + "/" + nexusGroupId.replace(/\./g, "/") + "/<%= pkg.name %>/<%= pkg.version %>-SNAPSHOT/<%= pkg.name %>-<%= pkg.version %>-SNAPSHOT.zip",
-                codePage: "UTF8"
+        uglify: {
+            uglifyTmp: {
+                files: [
+                    {
+                        expand: true,
+                        src: "**/*.js",
+                        dest: tmpDir,
+                        cwd: webAppDir,
+                        filter: function(filepath) {
+                            // prevent js from localService to be copied
+                            return !filepath.match(new RegExp(webAppDir + "(\\/|\\\\)localService", "gi"));
+                        }
+                    }]
+            },
+            uglifyPreload: {
+                files: [
+                    {
+                        expand: true,
+                        src: tmpDir + "/Component-preload.js"
+                    }]
             }
         },
-        releaseTransport: {
-            options: {
-                conn: abapConn
+        cssmin: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        src: "**/*.css",
+                        dest: tmpDir,
+                        cwd: webAppDir
+                    }]
+            }
+        },
+        openui5_preload: {
+            preloadDbg: {
+                options: {
+                    resources: {
+                        cwd: tmpDirDbg,
+                        src: ["**/*.js"],
+                        prefix: preloadPrefix
+                    },
+                    compress: false,
+                    dest: tmpDirDbg
+                },
+                components: true
+            },
+            preloadTmp: {
+                options: {
+                    resources: {
+                        cwd: tmpDir,
+                        src: ["**/*.js"],
+                        prefix: preloadPrefix
+                    },
+                    compress: false,
+                    dest: tmpDir
+                },
+                components: true
+            }
+        },
+        nexusDeployer: {
+            build: {
+                options: {
+                    groupId: nexusGroupId,
+                    artifactId: "<%= pkg.name %>",
+                    version: "<%= pkg.version %>-SNAPSHOT",
+                    packaging: "zip",
+                    auth: {
+                        username: nexusUser,
+                        password: nexusPassword
+                    },
+                    pomDir: targetDir + "/pom",
+                    url: nexusSnapshotRepoURL,
+                    uploadMetadata: false,
+                    artifact: targetDir + "/<%= pkg.name %>" + zipFileSuffix
+                }
+            }
+        },
+        zip: {
+            build: {
+                cwd: tmpDir,
+                src: tmpDir + "/**/*",
+                dest: targetDir + "/<%= pkg.name %>" + zipFileSuffix
             }
         }
     });
 
-    var rfcConnect = function(functionModule, importParameters, gruntContext) {
-        return new Promise(function(resolve, reject) {
-            var conn = gruntContext.options().conn;
-            var client = new rfc.Client(conn);
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-encoding");
+    grunt.loadNpmTasks("grunt-zip");
+    grunt.loadNpmTasks("grunt-openui5");
+    grunt.loadNpmTasks("grunt-nexus-deployer");
+    grunt.loadNpmTasks("grunt-eslint");
 
-            grunt.log.writeln("RFC client lib version:", client.getVersion());
-
-            client.connect(function(err) {
-                if (err) { // check for login/connection errors
-                    grunt.log.errorlns("could not connect to server", err);
-                    return reject();
-                }
-                // invoke remote enabled ABAP function module
-                grunt.log.writeln("Invoking function module", functionModule);
-                client.invoke(functionModule,
-                    importParameters,
-                    function(err, res) {
-                        if (err) { // check for errors (e.g. wrong parameters)
-                            grunt.log.errorlns("Error invoking", functionModule, err);
-                            return reject();
-                        }
-                        client.close();
-                        grunt.log.writeln("Messages:", res.EV_LOG_MESSAGES);
-                        return resolve(res);
-                    });
-            });
-        });
-    };
-
-
-    grunt.registerTask("createTransportRequest", "Creates an ABAP Transport Request", function() {
-        grunt.log.writeln("Creating Transport Request");
-        var importParameters = {
-            AUTHOR: this.options().author,
-            TEXT: this.options().description
-        };
-        var done = this.async();
-        rfcConnect("BAPI_CTREQUEST_CREATE", importParameters, this)
-            .then(
-            function(returnValue) {
-                if (returnValue.EV_SUCCESS == "E" || returnValue.EV_SUCCESS == "W") {
-                    grunt.log.errorlns("Error invoking BAPI_CTREQUEST_CREATE.");
-                    grunt.log.errorlns("Message Id:", returnValue.EV_MSG_ID);
-                    grunt.log.errorlns("Message No:", returnValue.EV_MSG_NO);
-                    grunt.log.errorlns("Messages:", returnValue.EV_LOG_MESSAGES);
-                    done(false);
-                    return;
-                }
-                if (returnValue.REQUESTID == "") {
-                    grunt.log.errorlns("Error invoking BAPI_CTREQUEST_CREATE.");
-                    grunt.log.errorlns("Transport request could not be created.");
-                    grunt.log.errorlns(returnValue.RETURN.MESSAGE);
-                    done(false);
-                    return;
-                }
-                grunt.log.writeln("Transport request", returnValue.REQUESTID, "created.");
-                if (fs.existsSync(targetDir) === false) {
-                    fs.mkdirSync(targetDir);
-                }
-                fs.writeFile(ctsDataFile,
-                    JSON.stringify(
-                        { REQUESTID: returnValue.REQUESTID }
-                    ),
-                    function(err) {
-                        if (err) {
-                            grunt.log.errorlns("Error Creating file:", err);
-                            done(false);
-                            return;
-                        }
-                        grunt.log.writeln("Created file:", ctsDataFile);
-                        done();
-                    }
-                );
-            },
-            function() {
-                done(false);
-            });
-    });
-
-    grunt.registerTask("uploadToABAP", "Uploads the application to the ABAP System", function(transportRequest) {
-        grunt.log.writeln("Uploading to ABAP");
-        if (!transportRequest) {
-            if (!fs.existsSync(ctsDataFile)) {
-                grunt.log.errorlns("No Transport request specified. Pass one explicitly or run createTransportRequest first.");
-                return (false);
-            }
-            transportRequest = JSON.parse(fs.readFileSync(ctsDataFile, { encoding: "utf8" })).REQUESTID;
-        }
-        grunt.log.writeln("Transport request:", transportRequest);
-        var url = "";
-        if (!(typeof this.options().zipFile === "undefined") && fs.existsSync(this.options().zipFile)) {
-            url = jobURL + "/ws/" + this.options().zipFile;
-        }
-        else {
-            url = this.options().zipFileURL;
-        }
-        var importParameters = {
-            IV_URL: url,
-            IV_SAPUI5_APPLICATION_NAME: abapApplicationName,
-            IV_SAPUI5_APPLICATION_DESC: abapApplicationDesc,
-            IV_PACKAGE: abapPackage,
-            IV_WORKBENCH_REQUEST: transportRequest,
-            IV_TEST_MODE: "-",
-            IV_EXTERNAL_CODE_PAGE: this.options().codePage
-        };
-        var done = this.async();
-        grunt.log.writeln("Uploading application from", url);
-        rfcConnect("/UI5/UI5_REPOSITORY_LOAD_HTTP", importParameters, this)
-            .then(
-            function(returnValue) {
-                if (returnValue.EV_SUCCESS == "E" || returnValue.EV_SUCCESS == "W") {
-                    grunt.log.errorlns("Error invoking", "/UI5/UI5_REPOSITORY_LOAD_HTTP");
-                    grunt.log.errorlns("Message Id:", returnValue.EV_MSG_ID);
-                    grunt.log.errorlns("Message No:", returnValue.EV_MSG_NO);
-                    grunt.log.errorlns("Messages:", returnValue.EV_LOG_MESSAGES);
-                    done(false);
-                    return;
-                }
-                grunt.log.writeln("Application uploaded.");
-                done();
-            },
-            function() {
-                done(false);
-            });
-    });
-
-    grunt.registerTask("releaseTransport", "Releases an ABAP Transport Request", function(transportRequest) {
-        grunt.log.writeln("Releasing Transport Request");
-        if (!transportRequest) {
-            if (!fs.existsSync(ctsDataFile)) {
-                grunt.log.errorlns("No Transport request specified. Pass one explicitly or run createTransportRequest first.");
-                return (false);
-            }
-            transportRequest = JSON.parse(fs.readFileSync(ctsDataFile, { encoding: "utf8" })).REQUESTID;
-        }
-        grunt.log.writeln("Transport request:", transportRequest);
-        var importParameters = {
-            REQUESTID: transportRequest,
-            COMPLETE: "X",
-            BATCH_MODE: "X"
-        };
-        var done = this.async();
-        rfcConnect("BAPI_CTREQUEST_RELEASE", importParameters, this)
-            .then(
-            function(returnValue) {
-                if (returnValue.EV_SUCCESS == "E" || returnValue.EV_SUCCESS == "W") {
-                    grunt.log.errorlns("Error invoking", "BAPI_CTREQUEST_RELEASE");
-                    grunt.log.errorlns("Message Id:", returnValue.EV_MSG_ID);
-                    grunt.log.errorlns("Message No:", returnValue.EV_MSG_NO);
-                    grunt.log.errorlns("Messages:", returnValue.EV_LOG_MESSAGES);
-                    done(false);
-                    return;
-                }
-                grunt.log.writeln("Transport request released.");
-                done();
-            },
-            function() {
-                done(false);
-            });
-    });
+    grunt.registerTask("default", ["clean", "copy:copyToDbg", "openui5_preload:preloadDbg", "copy:copyToTmp",
+          "uglify:uglifyTmp", "cssmin", "openui5_preload:preloadTmp", "copy:copyDbgToTmp",
+          "uglify:uglifyPreload"]);
+    grunt.registerTask("createZip", ["zip"]);
+    grunt.registerTask("deployToNexus", ["nexusDeployer"]);
 };
